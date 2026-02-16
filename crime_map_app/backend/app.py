19a41f880f061d8be88a4ced4639c3e63dfa3854
Alaedine Ait bella <aaitbell@bradford.ac.uk>
2026-02-16 02:43:21 +0000

    V1.1

--- crime_map_app/backend/app.py
+++ crime_map_app/backend/app.py
@@ -39,6 +39,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "https://YOUR-NETLIFY-SITE.netlify.app",
     ],
     allow_credentials=False,
     allow_methods=["*"],
--- crime_map_app/frontend/app.js
+++ crime_map_app/frontend/app.js
@@ -1,4 +1,5 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://crime-prediction-apii.onrender.com";
+
 
 let map, hotspotLayer;
 
